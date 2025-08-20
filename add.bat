e5a270edd341984254abbee8c54a2b35f4801353
Durgasankar Bandla <113372524+Durgasankar-99@users.noreply.github.com>
2025-08-20 14:28:24 +0530

    Update add.bat

--- add.bat
+++ add.bat
@@ -1,7 +1,6 @@
 @echo off
 :: Batch script to add two numbers passed as arguments
-:: %1 = first number
-:: %2 = second number
+:: %1 = first number, %2 = second number
 
 set /a result=%1+%2
 echo The sum of %1 and %2 is %result%
